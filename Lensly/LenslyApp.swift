fab5c3f314bc10480f8ac0809486b71ef6216952
bubiryov <bubiryov@icloud.com>
2024-02-22 17:41:28 +0200

    Initial Commit

---
+++ Lensly/LenslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenslyApp.swift
+//  Lensly
+//
+//  Created by Egor Bubiryov on 22.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct LenslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
